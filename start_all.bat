556f51c40d124c51320c2659e05956b4a53fec8d
XPGGC <3088084392@qq.com>
2025-06-28 22:44:36 +0800

    new gui, new feel

--- start_all.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM 启动 Flask 后端（指定 Anaconda 环境 python 路径）
-start cmd /k "cd /d %~dp0 && D:\Anaconda3\envs\wzry\python.exe app.py"
-
-REM 启动 React 前端
-start cmd /k "cd /d %~dp0\face-recognition-app && npm start"
-
-pause
